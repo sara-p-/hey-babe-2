606556641742b08ed25d0116fa7973d0ee338f63
Sara Pitt <sara@mightily.com>
2021-03-13 14:55:23 -0500

    testing things out again

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Hey babe.
+          Hello World!
         </h1>
 
         <p className={styles.description}>
