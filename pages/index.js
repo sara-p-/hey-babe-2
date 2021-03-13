e1aaddf0ae34e6d29b3029bf9943a72ba44fa242
Sara Pitt <sara@mightily.com>
2021-03-13 15:06:55 -0500

    updating the scss

--- pages/index.js
+++ pages/index.js
@@ -3,63 +3,179 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Hello World!
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+    <main>
+  <div className="info-tab">
+    <div className="info-wrapper">
+      <a href="#" className="info"><span className="visually-hidden">What is this?</span><i aria-hidden="true" className="icon far fa-question-circle"></i></a>
     </div>
+  </div>
+  <div className="info-box">
+    <div className="wrapper">
+      <div className="content">
+        <a href="#" className="close">
+          <span className="visually-hidden">Close info box</span>
+          <span className="close-lines">
+            <span className="close-line line1"></span>
+            <span className="close-line line2"></span>
+          </span>
+        </a>
+        <div className="question">
+          <h2>What is this?</h2>
+          <p>This is a birthday present for my partner. Each slide is something they love. Please enjoy.</p>
+        </div>
+        <div className="question">
+          <h2>How does it work?</h2>
+          <p>It’s a combination of illustrations made in Adobe Illustrator, and animations made with GreenSock.</p>
+        </div>
+        <div className="question">
+          <h2>Who are you?</h2>
+          <p>I'm a web developer and artist.</p>
+        </div>
+        <div className="question">
+          <h2>Can I see other things you've done?</h2>
+          <p>Abso-fucking-lutly. Go here: <a target="_blank" href="sara-pitt.com">sara-pitt.com</a></p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <section className="slide section-1">
+    <figure className="image peacock-2 from-bottom">
+      <img src="https://assets.codepen.io/2418359/peacock2.svg" />
+    </figure>
+    <figure className="image peacock-1 from-bottom">
+      <img src="https://assets.codepen.io/2418359/peacock1.svg" />
+    </figure>
+    <figure className="image chinese-evergreen-6 from-bottom">
+      <img src="https://assets.codepen.io/2418359/chinese-evergreen6.svg" />
+    </figure>
+    <figure className="image alocasia-1 from-bottom">
+      <img src="https://assets.codepen.io/2418359/alocasia2.svg" />
+    </figure>
+    <figure className="image banana-1 from-bottom">
+      <img src="https://assets.codepen.io/2418359/banana4.svg" />
+    </figure>
+    <figure className="image chinese-evergreen-5 from-bottom">
+      <img src="https://assets.codepen.io/2418359/chinese-evergreen5-2.svg" />
+    </figure>
+    <figure className="image watermelon-plant-1 from-bottom">
+      <img src="https://assets.codepen.io/2418359/watermelon-plant1.svg" />
+    </figure>
+    <figure className="image chinese-evergreen-1 from-bottom">
+      <img src="https://assets.codepen.io/2418359/chinese-evergreen1-2.svg" />
+    </figure>
+    <figure className="image croton-1 from-bottom">
+      <img src="https://assets.codepen.io/2418359/croton1.svg" />
+    </figure>
+    <figure className="image rattlesnake-1 from-bottom">
+      <img src="https://assets.codepen.io/2418359/rattlesnake2-2.svg" />
+    </figure>
+    <figure className="image chinese-evergreen-4 from-bottom">
+      <img src="https://assets.codepen.io/2418359/chinese-evergreen4.svg" />
+    </figure>
+    <figure className="image chinese-evergreen-2 from-bottom">
+      <img src="https://assets.codepen.io/2418359/chinese-evergreen2.svg" />
+    </figure>
+    <figure className="image snake-plant-1 from-bottom">
+      <img src="https://assets.codepen.io/2418359/snake-plant1.svg" />
+    </figure>
+    <figure className="image monstera-1 from-bottom">
+      <img src="https://assets.codepen.io/2418359/monstera2-2.svg" />
+    </figure>
+    <figure className="image fiddle-leaf-1 from-bottom">
+      <img src="https://assets.codepen.io/2418359/fiddle-leaf1.svg" />
+    </figure>
+    <figure className="image chinese-evergreen-3 from-bottom">
+      <img src="https://assets.codepen.io/2418359/chinese-evergreen3.svg" />
+    </figure>
+    <figure className="image banana-2 from-bottom">
+      <img src="https://assets.codepen.io/2418359/banana2-2.svg" />
+    </figure>
+    <figure className="image rubber-tree-1 from-bottom">
+      <img src="https://assets.codepen.io/2418359/rubber-tree1.svg" />
+    </figure>
+    <figure className="image rattlesnake-2">
+      <img src="https://assets.codepen.io/2418359/rattlesnake1.svg" />
+    </figure>
+    <figure className="image rattlesnake-3">
+      <img src="https://assets.codepen.io/2418359/rattlesnake4.svg" />
+    </figure>
+    <figure className="image monstera-2">
+      <img src="https://assets.codepen.io/2418359/monstera3.svg" />
+    </figure>
+    <figure className="image nerve-plant-1">
+      <img src="https://assets.codepen.io/2418359/nerve-plant.svg" />
+    </figure>
+    <figure className="image succulent-1">
+      <img src="https://assets.codepen.io/2418359/succulent1.svg" />
+    </figure>
+    <figure className="image pothos-1">
+      <img src="https://assets.codepen.io/2418359/pothos2.svg" />
+    </figure>
+    <figure className="image palm-1">
+      <img src="https://assets.codepen.io/2418359/palm1.svg" />
+    </figure>
+    <figure className="image string-of-pearls-1">
+      <img src="https://assets.codepen.io/2418359/string-of-pearls2.svg" />
+    </figure>
+    <figure className="image flower-1">
+      <img src="https://assets.codepen.io/2418359/flower2.svg" />
+    </figure>
+
+    <div className="border border-top"></div>
+    <div className="border border-right"></div>
+    <div className="border border-bottom"></div>
+    <div className="border border-left"></div>
+
+    <div className="section-content">
+      <div className="wrapper">
+        <div className="content">
+          <h1 className="hey-babe">
+            <span className="h-group letter-group">
+              <span className="letter h-letter letter-1">h</span>
+              <span className="letter h-letter letter-2">h</span>
+              <span className="letter h-letter letter-3">h</span>
+            </span>
+            <span className="e-group letter-group">
+              <span className="letter e-letter letter-1">e</span>
+              <span className="letter e-letter letter-2">e</span>
+              <span className="letter e-letter letter-3">e</span>
+            </span>
+            <span className="y-group letter-group">
+              <span className="letter y-letter letter-1">y</span>
+              <span className="letter y-letter letter-2">y</span>
+              <span className="letter y-letter letter-3">y</span>
+            </span>
+            <span className="space-group letter-group"> </span>
+            <span className="b-group letter-group">
+              <span className="letter b-letter letter-1">b</span>
+              <span className="letter b-letter letter-2">b</span>
+              <span className="letter b-letter letter-3">b</span>
+            </span>
+            <span className="a-group letter-group">
+              <span className="letter a-letter letter-1">a</span>
+              <span className="letter a-letter letter-2">a</span>
+              <span className="letter a-letter letter-3">a</span>
+            </span>
+            <span className="b2-group letter-group">
+              <span className="letter b2-letter letter-1">b</span>
+              <span className="letter b2-letter letter-2">b</span>
+              <span className="letter b2-letter letter-3">b</span>
+            </span>
+            <span className="e2-group letter-group">
+              <span className="letter e2-letter letter-1">e</span>
+              <span className="letter e2-letter letter-2">e</span>
+              <span className="letter e2-letter letter-3">e</span>
+            </span>
+            <span className="period-group letter-group">
+              <span className="letter period-letter letter-1">.</span>
+              <span className="letter period-letter letter-2">.</span>
+              <span className="letter period-letter letter-3">.</span>
+            </span>
+          </h1>
+        </div>
+      </div>
+    </div>
+  </section>
+</main>
   )
 }
